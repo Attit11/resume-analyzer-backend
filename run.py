c3237589cda60e6cf2cefe72ae019da01133f817
Attit11 <sharmaattit09@gmail.com>
2026-03-09 22:42:14 -0400

    added procfile

--- run.py
+++ run.py
@@ -40,5 +40,4 @@ with app.app_context():
         print("Database connection failed:", e)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
